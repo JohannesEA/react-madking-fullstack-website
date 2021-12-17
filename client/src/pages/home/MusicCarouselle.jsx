36bce9a9f10a0ed0096cd258e933636917fe0bd4
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-17 10:37:13 +0100

    Add new route to single product

--- client/src/pages/home/MusicCarouselle.jsx
+++ client/src/pages/home/MusicCarouselle.jsx
@@ -27,7 +27,7 @@ const MusicCarouselle = () => {
   const hoverColor = "#767676";
 
   const handleClick = (item) => {
-    path = "api/products/find/" + item._id;
+    path = "/products/" + item._id;
     navigate(path);
   };
 
