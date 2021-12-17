2ccaa8e23408407cbeed20e08cdf7be5f110ffdc
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-17 10:44:22 +0100

    Add correct auth path for login

--- client/src/redux/apiCalls.js
+++ client/src/redux/apiCalls.js
@@ -19,7 +19,7 @@ import {
 export const login = async (dispatch, user) => {
     dispatch(loginStart());
     try {
-        const res = await publicRequest.post("/auth/login", user);
+        const res = await publicRequest.post("api/auth/login", user);
         dispatch(loginSuccess(res.data));
     } catch (err) {
         dispatch(loginFailure());
