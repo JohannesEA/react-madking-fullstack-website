3d1d7c9edc7a342e0df9505dc4cc9c5ad94987bd
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-18 15:17:42 +0100

    Add buttons to slider

--- client/src/pages/home/MusicCarouselle.jsx
+++ client/src/pages/home/MusicCarouselle.jsx
@@ -83,13 +83,13 @@ const MusicCarouselle = () => {
                 cursor: "pointer",
                 border: "none",
                 borderRadius: "1.5em "
-              }}>Back</ButtonBack>
+              }}>Forrige</ButtonBack>
               <ButtonNext style={{
                 color: "black", zIndex: 1, padding: "14px 18px", margin: "1em .5em", transition: "all 0.2s ease", fontWeight: "600",
                 cursor: "pointer",
                 border: "none",
                 borderRadius: "1.5em "
-              }}>Next</ButtonNext>
+              }}>Neste</ButtonNext>
             </Slide>
           ))}
         </Slider>
