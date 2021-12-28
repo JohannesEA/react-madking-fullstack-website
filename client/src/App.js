1b8bd84b520ba80234a015c78c5d7765a4d58642
Johannes Erdahl Andresen <johannes.erdahl.andresen@gmail.com>
2021-12-29 00:43:52 +0100

    Add loading screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,5 @@
+import React, { useEffect } from "react";
+import styled, { keyframes } from "styled-components";
 import {
     BrowserRouter as Router,
     Route,
@@ -11,32 +13,86 @@ import Productpage from "./pages/product/Productpage";
 import Orderpage from "./pages/order/Orderpage";
 import Loginpage from "./pages/login/Loginpage";
 import ProductListPage from "./pages/productList/ProductListPage";
-import { useSelector } from "react-redux";
 import Adminpage from "./pages/admin/home/Adminpage";
 import AdminProductpage from "./pages/admin/products/AdminProductPage";
+import { useDispatch, useSelector } from "react-redux";
+import { getContent } from "./redux/apiCalls";
 
 const App = () => {
     const user = useSelector((state) => state.user.currentUser);
     console.log(user);
+    const content = useSelector((state) => state.content.contents);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        getContent(dispatch);
+    }, [dispatch]);
 
     return (
         <Router>
             <GlobalStyles />
-            <Routes>
-                <Route path="/" element={<Homepage />} />
-                <Route path="/products/:id" element={<Productpage />} />
-                <Route path="/productlist/" element={<ProductListPage />} />
-                <Route path="/orders/" element={<Orderpage />} />
-                <Route
-                    path="/login"
-                    element={user ? <Navigate to="/" /> : <Loginpage />}
-                />
-                <Route path="/admin" element={<Adminpage />} />
-                <Route path="/producthandler" element={<AdminProductpage />} />
-
-            </Routes>
+            {content.length > 0 ? (
+                <Routes>
+                    <Route path="/" element={<Homepage />} />
+                    <Route path="/products/:id" element={<Productpage />} />
+                    <Route path="/productlist/" element={<ProductListPage />} />
+                    <Route path="/orders/" element={<Orderpage />} />
+                    <Route
+                        path="/login"
+                        element={user ? <Navigate to="/" /> : <Loginpage />}
+                    />
+                    <Route path="/admin" element={<Adminpage />} />
+                    <Route
+                        path="/producthandler"
+                        element={<AdminProductpage />}
+                    />
+                </Routes>
+            ) : (
+                <Container>
+                    <Image
+                        src="/assets/logos/logo.png"
+                        alt="loading-logo"
+                        width="230"
+                        height="230"
+                    />
+                    <Text>Loading..</Text>
+                </Container>
+            )}
         </Router>
     );
 };
 
 export default App;
+
+const animation = keyframes`
+100%{
+    transform: rotate(360deg);
+
+}
+`;
+
+const Container = styled.div`
+    height: 10em;
+    width: 10em;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    color: white;
+    margin: 10em auto;
+
+    @media (max-width: 800px) {
+        height: 5em;
+        width: 5em;
+    }
+`;
+
+const Image = styled.img`
+    flex-shrink: 0;
+    border-radius: 100em;
+    animation: ${animation} 2s linear infinite;
+    min-width: 100%;
+    min-height: 100%;
+`;
+
+const Text = styled.p``;
